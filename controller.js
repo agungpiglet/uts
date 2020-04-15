1022914a2131830dfa66b6d0ac247ce114d6b1b1
unknown <agungpiglet@gmail.com>
2020-04-15 10:33:02 +0700

    menampilkan data di tabel service

--- controller.js
+++ controller.js
@@ -6,3 +6,14 @@ var connection = require('./koneksi');
 exports.index = function(req, res){
     response.ok("Aplikasi saya berjalan!!",res)
 };
+
+
+exports.tampilsemuadata = function(req,res){
+    connection.query('SELECT * FROM t_service', function(error,rows,fileds){
+        if(error){
+            connection.log(error);
+        }else {
+            response.ok(rows,res)
+        }
+    });
+};
