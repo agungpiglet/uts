8260345e22443592fd79323a36198756115e4f60
Agung Piglet <agungpiglet@gmail.com>
2020-05-16 23:12:12 +0700

    regist berhasil

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-Parser');
+
+var morgan = require('morgan');
 const app = express();
 
 
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(morgan('dev'));
 
 
 var routes = require('./routes');
 routes(app);
+
+//daftar menu routes
+app.use('/auth', require('./middleware'));
 
 app.listen(3000, () => {
     console.log(`Server started on port`);
